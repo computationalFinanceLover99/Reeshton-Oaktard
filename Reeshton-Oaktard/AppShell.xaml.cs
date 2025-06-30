dee0881568e53abed0a162a9c141f60825a36a77
doureesha <doureesha@gmail.com>
2025-06-30 05:56:48 -0700

    initial commit

---
+++ Reeshton-Oaktard/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Reeshton_Oaktard
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Reeshton-Oaktard/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Reeshton_Oaktard
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
